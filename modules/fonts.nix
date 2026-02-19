0ef45ba9af787292cc24b80df0e16756111aee1d
SMLnobita <nguyentranquihoaacek4@gmail.com>
2026-02-19 12:03:46 +0700

    update font

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,16 +1,35 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   fonts = {
     fontDir.enable = true;
-
     packages = with pkgs; [
       nerd-fonts.jetbrains-mono
+      ubuntu-classic
     ];
 
     fontconfig.defaultFonts = {
-      monospace = [ "JetBrainsMono Nerd Font Mono" ];
+      monospace = ["JetBrainsMono Nerd Font Mono"];
+      sansSerif = ["Ubuntu"];
+      serif = ["Ubuntu"];
     };
   };
-}
 
+  programs.dconf = {
+    enable = true;
+    profiles.user.databases = [
+      {
+        settings = {
+          "org/gnome/desktop/interface" = {
+            font-name = "Ubuntu 12";
+            document-font-name = "Ubuntu 12";
+            monospace-font-name = "JetBrainsMono Nerd Font Mono 12";
+            text-scaling-factor = 1.0;
+          };
+
+          "org/gnome/desktop/wm/preferences" = {
+            titlebar-font = "Ubuntu Bold 12";
+          };
+        };
+      }
+    ];
+  };
+}
