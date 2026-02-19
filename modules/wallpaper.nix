f32fbff1dbf0a2f6290b296f029bb43e62fd2593
SMLnobita <nguyentranquihoaacek4@gmail.com>
2026-02-19 17:00:52 +0700

    update

--- modules/wallpaper.nix
+++ modules/wallpaper.nix
@@ -9,34 +9,43 @@
 in {
   programs.dconf.enable = true;
 
-  services.desktopManager.gnome = {
-    extraGSettingsOverridePackages = [
-      pkgs.gsettings-desktop-schemas
-    ];
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/desktop/background" = {
+          picture-uri = "file://${light}";
+          picture-uri-dark = "file://${dark}";
+          picture-options = "zoom";
+          color-shading-type = "solid";
+        };
 
-    extraGSettingsOverrides = ''
-      [org.gnome.desktop.background]
-      picture-uri='file://${light}'
-      picture-uri-dark='file://${dark}'
-      picture-options='zoom'
-      color-shading-type='solid'
+        "org/gnome/desktop/screensaver" = {
+          picture-uri = "file://${dark}";
+        };
 
-      [org.gnome.desktop.screensaver]
-      picture-uri='file://${dark}'
+        "org/gnome/desktop/interface" = {
+          enable-hot-corners = true;
+          show-battery-percentage = true;
+          clock-show-weekday = true;
+          clock-show-seconds = false;
+          enable-animations = true;
+          gtk-enable-primary-paste = false;
+        };
 
-      [org.gnome.desktop.interface]
-      enable-hot-corners=true
-      show-battery-percentage=true
-      clock-show-weekday=true
-      clock-show-seconds=false
-      enable-animations=true
-      gtk-enable-primary-paste=false
+        "org/gnome/mutter" = {
+          dynamic-workspaces = true;
+          edge-tiling = true;
+          center-new-windows = true;
+          experimental-features = ["scale-monitor-framebuffer"];
+        };
 
-      [org.gnome.mutter]
-      dynamic-workspaces=true
-      edge-tiling=true
-      center-new-windows=true
-      experimental-features=['scale-monitor-framebuffer']
-    '';
-  };
+        "org/gnome/shell" = {
+          disable-user-extensions = false;
+          enabled-extensions = [
+            "user-theme@gnome-shell-extensions.gcampax.github.com"
+          ];
+        };
+      };
+    }
+  ];
 }
