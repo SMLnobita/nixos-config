e51ac355b5c1b9e18efd9b4f8b98bc9da1a1ff94
SMLnobita <nguyentranquihoaacek4@gmail.com>
2026-02-20 07:02:24 +0700

    cai zsh

--- configuration.nix
+++ configuration.nix
@@ -133,6 +133,9 @@
     wezterm
   ];
 
+  # Shell by nguyenhoa
+  programs.zsh.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
